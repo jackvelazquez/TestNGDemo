bf65d2bc72752604de0b6d1cd6c39e230699fe98
Jack <jose.velazquez@gmail.com>
2021-02-22 18:12:22 -0600

    Commit 6:15pm

--- src/test/java/JAC/SeleniumTest.java
+++ src/test/java/JAC/SeleniumTest.java
@@ -11,11 +11,30 @@ import org.testng.annotations.*;
 
 public class SeleniumTest {
 
-    public static WebDriver driver;
+    @BeforeGroups
+    public void  BeforeGroupsFunc() { System.out.println("Before Groups..."); }
 
+    @BeforeSuite
+    public void  BeforeSuiteFunc() { System.out.println("Before Suite..."); }
+
+    @BeforeTest
+    public void  BeforeTestFunc() { System.out.println("Before Test..."); }
+
+    @BeforeMethod
+    public void  BeforeMethodFunc() { System.out.println("Before Method..."); }
+
+    @BeforeClass
+    public void  BeforeClassFunc() { System.out.println("Before Class..."); }
+
+    // Main TESTPLAN
     @Test
-    public void  AmazonTitleTest()
+    @Parameters({"param1"})
+    public void  AmazonTitleTest(String param1)
     {
+        //Starts the web driver
+        WebDriver driver;
+
+        System.out.println("Parameter passed to AmazonTitleTest --> " + param1);
         System.out.println("Test Started...");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -48,5 +67,30 @@ public class SeleniumTest {
         System.out.println("Test Completed");
     }
 
+    @Test
+    public void  SimpleTest1()
+    {
+        System.out.println("Simple Test Doing nothing...");
+        Assert.assertTrue(true);
+    }
+
+    // END of Main Execution Plan
+
+
+    @AfterMethod
+    public void  AfterMethodFunc() { System.out.println("After Method...");  }
+
+    @AfterClass
+    public void  AfterClassFunc() { System.out.println("After Class..."); }
+
+    @AfterTest
+    public void  AfterTestFunc() {  System.out.println("After Test..."); }
+
+    @AfterSuite
+    public void  AfterSuiteFunc()
+    { System.out.println("After Suite..."); }
+
+    @AfterGroups
+    public void  AfterGroupsFunc() { System.out.println("After Groups..."); }
 
 }
